8b102524d18dafde72831d1ded80062b00d36deb
Thabiso Mapoulo <manuelthabisomapoulo@gmail.com>
2025-09-30 20:22:46 +0200

    added the stripe payment gateway

--- shuttle-booking-system/app.js
+++ shuttle-booking-system/app.js
@@ -7,21 +7,24 @@ const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var signinRouter = require('./routes/signin'); // matches file name
-const shuttleRoutes = require('./routes/shuttle');
+var signinRouter = require('./routes/signin'); // login
+var shuttleRoutes = require('./routes/shuttle');
+var bookingRoutes = require('./routes/booking');
+var paymentRoutes = require('./routes/payment');
+var stripePayRouter = require('./routes/stripe-pay'); // Stripe payment
+
 var app = express();
+
 // CORS setup
 app.use(cors({
-  origin: 'http://localhost:5173', // your React app
+  origin: 'http://localhost:5173', // React frontend
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   credentials: true
 }));
 
-// Allow preflight for all routes
-app.options("*", cors());
+app.options("*", cors()); // Preflight
 
-
-// view engine setup
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -34,15 +37,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/users/login', signinRouter); // login route
+app.use('/users/login', signinRouter);
 app.use('/api/shuttles', shuttleRoutes);
+app.use('/api/bookings', bookingRoutes);
+app.use('/api/payments', paymentRoutes);
+app.use('/api/stripe-pay', stripePayRouter); // Stripe route
 
-// catch 404 and forward to error handler
+// Catch 404
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Error handler
 app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
